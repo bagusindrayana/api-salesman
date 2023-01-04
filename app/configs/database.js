ed9fddd3322e29d9bed5bd360aaf9537f9e10e9c
Bagus Indrayana <bagusindrayanaindo@gmail.com>
2023-01-04 16:59:48 +0800

    update db url

--- app/configs/database.js
+++ app/configs/database.js
@@ -1,5 +1,5 @@
 function mongo_uri() {
-    return process.env.MONGODB_URI ?? "mongodb+srv://mydbroot:Ghcj8Q4ijEZE0TSI@cluster0.ffdoorx.mongodb.net/db_tsp?retryWrites=true&w=majority";
+    return process.env.MONGODB_URI ?? "mongodb+srv://mydbroot:Ghcj8Q4ijEZE0TSI@cluster0.ffdoorx.mongodb.net/db_salesman?retryWrites=true&w=majority";
 }
 
 module.exports = {
